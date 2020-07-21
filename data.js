e95a074f8f76fdb532026ba54df0dc5c51cbfe6f
Farsheed <zirafa@users.noreply.github.com>
2020-07-21 17:13:28 -0500

    Update data.js

--- data.js
+++ data.js
@@ -113,8 +113,7 @@ function tableModifiers() {
     marker: {
       color:[],
       colorscale: [
-        ['0.0', 'rgb(230, 74, 25)'],    
-        ['0.5', 'rgb(255, 193, 7)'],    
+        ['0.0', 'rgb(255, 193, 7)'],    
         ['1.0', 'rgb(76, 175, 80)'] 
       ]
     }
@@ -161,7 +160,7 @@ function tableModifiers() {
         bar1.x.unshift(name);
         bar1.y.unshift(filtration);
         bar1.text.unshift(resistance);
-        bar1.marker.color.unshift(resistance)
+        bar1.marker.color.unshift(100-resistance)
       }
 
     }
